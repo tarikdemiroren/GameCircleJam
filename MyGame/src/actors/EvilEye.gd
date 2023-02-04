7cbe836f0202f6493b73b6b62d431a56176b55cb
Tarık Demirören <tarekarez5652@gmail.com>
2023-02-04 16:43:05 +0300

    evil eye enemy created

--- MyGame/src/actors/EvilEye.gd
+++ MyGame/src/actors/EvilEye.gd
@@ -6,10 +6,12 @@ onready var fliSprite = $EvilEyeFlight
 onready var hitSprite = $EvilEyeTakeHit
 onready var animator = $EvilEyeAnimation
 var direction = Vector2.ZERO
-var speed = 200
+var speed = 150
+var max_speed = 200
 var velocity = Vector2.ZERO
 var mybody
 var is_in_area = false
+var is_attack_on = false
 var steering
 
 # Declare member variables here. Examples:
@@ -26,10 +28,21 @@ func _ready():
 func _physics_process(delta: float):
 	if is_in_area:
 		direction = (mybody.global_position-self.global_position).normalized()*speed
-		print(mybody.position)
-	steering = direction - velocity
-	velocity = move_and_slide(velocity + steering)
-	
+		steering = direction - velocity
+		velocity = velocity + steering
+		velocity = move_and_slide(velocity)
+		if (velocity.x >= 0) and not is_attack_on:
+			animator.play("flight")
+		elif not is_attack_on:
+			animator.play("flight_left")
+		if (velocity.x >= 0) and is_attack_on:
+			animator.play("EvilEyeAttack")
+		elif is_attack_on:
+			animator.play("EvilEyeAttackLeft")
+
+	if speed > max_speed:
+		speed = speed - 10
+
 	pass
 
 
@@ -39,8 +52,28 @@ func _on_DetectRange_body_entered(body):
 	#direction = (body.position-self.position).normalized()
 	pass # Replace with function body.
 
-
 func _on_DetectRange_body_exited(body):
 	is_in_area = false
 	mybody = null
+	pass # Replace with function body.
+
+func _on_AttRange_body_entered(body):
+	is_attack_on = true
+	fliSprite.hide()
+	attSprite.show()
+	if (velocity.x >= 0) and is_attack_on:
+		animator.play("EvilEyeAttack")
+	elif is_attack_on:
+		animator.play("EvilEyeAttackLeft")
+	speed = 300
+	pass # Replace with function body.
+
+func _on_AttRange_body_exited(body):
+	is_attack_on=false
+	attSprite.hide()
+	fliSprite.show()
+	if (velocity.x > 0):
+		animator.play("flight")
+	else:
+		animator.play("flight_left")
 	pass # Replace with function body.
