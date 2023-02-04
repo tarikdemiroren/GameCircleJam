d80416bdbb4f71148dd6fba07687df20a54a10da
yavuz-ozturk <71942103+yavuz-ozturk@users.noreply.github.com>
2023-02-04 14:54:29 +0300

    banana

--- MyGame/src/actors/Player.gd
+++ MyGame/src/actors/Player.gd
@@ -3,13 +3,14 @@ extends Actor
 onready var cam : Camera2D = $Camera
 onready var animation_tree : AnimationTree = $PlayerAnimationTree
 onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
-onready var animation_player := $AnimationPlayer
+onready var animation_player := $playerAnimationController
 
 enum FACED_DIR {RIGHT, LEFT, UP, DOWN}
 var last_dir_updo = FACED_DIR.DOWN
 var last_dir_leri = FACED_DIR.RIGHT
 var input_direction = Vector2()
 
+var cutInput = false
 var isSprinting = false
 onready var rootSpawns = [$rootSpawner, $rootSpawner2, $rootSpawner3, $rootSpawner4, $rootSpawner5, $rootSpawner6, $rootSpawner7, $rootSpawner8, $rootSpawner9]
 
@@ -23,27 +24,34 @@ func _physics_process(delta):
 	input_direction = move_and_slide(input_direction)
 
 func get_input():
+	
 	input_direction = Vector2.ZERO
-	if Input.is_action_pressed("right"):
-		input_direction.x += 1
-	if Input.is_action_pressed("left"):
-		input_direction.x -= 1
-	if Input.is_action_pressed("down"):
-		input_direction.y += 1
-	if Input.is_action_pressed("up"):
-		input_direction.y -= 1
-	input_direction = input_direction.normalized() * speed
+	isSprinting = false
 	
-	if Input.is_action_pressed("sprint"):
-		isSprinting = true
-		input_direction = input_direction * 1.7
-	else:
-		isSprinting = false
+	if not cutInput:
+	
+		if Input.is_action_just_pressed("ultimate_attack"):
+			ultimate_attack()
+			cutInput = true
+			return
 		
-	if Input.is_action_just_pressed("ultimate_attack"):
-		ultimate_attack()
+		if Input.is_action_pressed("right"):
+			input_direction.x += 1
+		if Input.is_action_pressed("left"):
+			input_direction.x -= 1
+		if Input.is_action_pressed("down"):
+			input_direction.y += 1
+		if Input.is_action_pressed("up"):
+			input_direction.y -= 1
+		input_direction = input_direction.normalized() * speed
+		
+		if Input.is_action_pressed("sprint"):
+			isSprinting = true
+			input_direction = input_direction * 1.7
+		else:
+			isSprinting = false
 	
-	determine_move_animation()
+		determine_move_animation()
 	
 
 func determine_move_animation():
@@ -93,14 +101,21 @@ func determine_move_animation():
 			return
 
 func ultimate_attack():
-	state_machine.start("gaia_s_mercy")
-	print(state_machine.get_current_play_position())
+	state_machine.travel("gaia_s_mercy")
 	for item in rootSpawns:
 		item.spawn()
-	#summon trees
-
 	pass
 
+func free_soul():
+	cutInput = false
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+#func _on_playerAnimationController_animation_finished(anim_name: String) -> void:
+#	if anim_name == "gaia_s_mercy":
+#		for item in rootSpawns:
+#			item.spawn()
+	
+	
